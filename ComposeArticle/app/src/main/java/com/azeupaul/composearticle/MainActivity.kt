2c55740146e3605eda50bcb622c569ef9b7a8d32
azeupaul <azeupaul@gmail.com>
2022-10-21 11:11:57 +0100

    Completed exercise compose article

--- ComposeArticle/app/src/main/java/com/azeupaul/composearticle/MainActivity.kt
+++ ComposeArticle/app/src/main/java/com/azeupaul/composearticle/MainActivity.kt
@@ -3,13 +3,19 @@ package com.azeupaul.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.azeupaul.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Article(
+                        getString(R.string.article_title_text),
+                        getString(R.string.article_summary_text),
+                        getString(R.string.article_description_text))
                 }
             }
         }
@@ -30,14 +39,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Article(title: String, summary: String, description: String) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = summary,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ArticlePreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        Article(
+            stringResource(R.string.article_title_text),
+            stringResource(R.string.article_summary_text),
+            stringResource(R.string.article_description_text)
+        )
     }
 }

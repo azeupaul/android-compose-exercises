0d1fe67d8ad70ee977b93025151d6371291bab52
azeupaul <azeupaul@gmail.com>
2022-11-24 09:14:51 +0100

    Hero item card is finish

--- SuperHeroes/app/src/main/java/cm/azeupaul/superheroes/MainActivity.kt
+++ SuperHeroes/app/src/main/java/cm/azeupaul/superheroes/MainActivity.kt
@@ -3,13 +3,25 @@ package cm.azeupaul.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import cm.azeupaul.superheroes.model.HeroesRepository
+import cm.azeupaul.superheroes.model.SuperHero
 import cm.azeupaul.superheroes.ui.theme.SuperHeroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    
                 }
             }
         }
@@ -30,14 +42,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HeroItem(hero: SuperHero, modifier: Modifier = Modifier) {
+    Card(
+        elevation = 2.dp,
+        modifier = modifier,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .height(74.dp)
+        ) {
+            Column(
+            ) {
+                Text(
+                    text = stringResource(id = hero.nameRes),
+                    style = MaterialTheme.typography.h3
+                )
+                Text(
+                    text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+
+            Image(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .size(74.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                ,
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = hero.imageRes),
+                contentDescription = stringResource(id = hero.nameRes),
+
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SuperHeroesTheme {
-        Greeting("Android")
+        HeroItem(hero = HeroesRepository.heroes[1])
     }
 }

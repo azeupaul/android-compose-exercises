daf4994782713a3e69c3deab3f36941426894ae9
azeupaul <azeupaul@gmail.com>
2022-11-24 02:40:28 +0100

    Create model, data and string resources

---
+++ SuperHeroes/app/src/main/java/cm/azeupaul/superheroes/model/SuperHero.kt
@@ -0,0 +1,10 @@
+package cm.azeupaul.superheroes.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class SuperHero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
---
+++ SuperHeroes/app/src/main/java/cm/azeupaul/superheroes/model/data.kt
@@ -0,0 +1,38 @@
+package cm.azeupaul.superheroes.model
+
+import cm.azeupaul.superheroes.R
+
+object HeroesRepository {
+    val heroes = listOf(
+        SuperHero(
+            nameRes = R.string.hero1,
+            descriptionRes = R.string.description1,
+            imageRes = R.drawable.android_superhero1
+        ),
+        SuperHero(
+            nameRes = R.string.hero2,
+            descriptionRes = R.string.description2,
+            imageRes = R.drawable.android_superhero2
+        ),
+        SuperHero(
+            nameRes = R.string.hero3,
+            descriptionRes = R.string.description3,
+            imageRes = R.drawable.android_superhero3
+        ),
+        SuperHero(
+            nameRes = R.string.hero4,
+            descriptionRes = R.string.description4,
+            imageRes = R.drawable.android_superhero4
+        ),
+        SuperHero(
+            nameRes = R.string.hero5,
+            descriptionRes = R.string.description5,
+            imageRes = R.drawable.android_superhero5
+        ),
+        SuperHero(
+            nameRes = R.string.hero6,
+            descriptionRes = R.string.description6,
+            imageRes = R.drawable.android_superhero6
+        )
+    )
+}

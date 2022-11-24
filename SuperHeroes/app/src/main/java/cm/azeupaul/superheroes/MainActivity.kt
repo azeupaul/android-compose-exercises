1a43a55a8bd5264e95cb3d7b5cce992f547ec9f6
azeupaul <azeupaul@gmail.com>
2022-11-24 12:00:09 +0100

    Add top bar

--- SuperHeroes/app/src/main/java/cm/azeupaul/superheroes/MainActivity.kt
+++ SuperHeroes/app/src/main/java/cm/azeupaul/superheroes/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +17,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cm.azeupaul.superheroes.model.HeroesRepository
@@ -32,13 +31,40 @@ class MainActivity : ComponentActivity() {
             SuperHeroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    
+                    SuperHeroApp()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun SuperHeroApp()
+{
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .height(56.dp),
+                elevation = 0.dp,
+                backgroundColor = MaterialTheme.colors.background
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.h1,
+                        color = MaterialTheme.colors.onSurface
+                    )
+                }
+            }
+        }
+    ) {
+        HeroList(heroes = HeroesRepository.heroes)
     }
 }
 
@@ -67,7 +93,7 @@ fun HeroItem(hero: SuperHero, modifier: Modifier = Modifier) {
                 .padding(16.dp)
                 .height(74.dp)
         ) {
-            Column {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = stringResource(id = hero.nameRes),
                     style = MaterialTheme.typography.h3
@@ -95,8 +121,17 @@ fun HeroItem(hero: SuperHero, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    SuperHeroesTheme {
-        HeroList(heroes = HeroesRepository.heroes)
+fun AppPreview() {
+    SuperHeroesTheme() {
+        SuperHeroApp()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AppDarkPreview() {
+    SuperHeroesTheme(darkTheme = true) {
+        SuperHeroApp()
     }
 }

6ccb15c24b8acc2ad2d3abe52f74c7f2d4aa8b0b
azeupaul <azeupaul@gmail.com>
2022-11-26 23:31:32 +0100

    Design card item food

--- Daily30CamerounianFoods/app/src/main/java/cm/azeupaul/daily30camerounianfoods/MainActivity.kt
+++ Daily30CamerounianFoods/app/src/main/java/cm/azeupaul/daily30camerounianfoods/MainActivity.kt
@@ -3,13 +3,25 @@ package cm.azeupaul.daily30camerounianfoods
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cm.azeupaul.daily30camerounianfoods.ui.theme.Daily30CamerounianFoodsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,14 +42,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ItemFood(modifier: Modifier = Modifier) {
+    var expanded by remember {
+      mutableStateOf(false)
+    }
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            ),
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+        ) {
+            Row(
+                modifier
+                    .fillMaxWidth()
+                    .height(30.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Text(
+                    text = "Day 1 - Poulet Dg"
+                )
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(
+                        imageVector = if(expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                        contentDescription = "Dropdown button"
+                    )
+                }
+
+            }
+            if(expanded){
+                Spacer(Modifier.height(12.dp))
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(RoundedCornerShape(16.dp)),
+                    painter = painterResource(id = R.drawable.poulet_dg),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "Poulet DG",
+                )
+                Spacer(Modifier.height(12.dp))
+                Text(text = "DG pour \"Directeur Général\" car c'est un plat qui est souv" +
+                        "ent cuisiné pour recevoir les personnalités. Ce plat composé de poulet, " +
+                        "de bananes plantain, de légumes et de condiments est un véritable délice " +
+                        "qui fait toujours l'unanimité et retiendra votre mari à la maison, " +
+                        "ne serait-ce qu'une soirée."
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Daily30CamerounianFoodsTheme {
-        Greeting("Android")
+        ItemFood()
     }
 }
